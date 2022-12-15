0bec46d33ce58aac0a0a3b4dc2ae05f8aaed1da2
江程训 <dfjsiou@qq.com>
2022-12-15 16:22:12 +0800

    任务一弄好了，撒花花~

--- 期末作业/内容/任务一/main.py
+++ 期末作业/内容/任务一/main.py
@@ -9,21 +9,29 @@ def calculate_grade(regular_grade, final_grade):
 def read_csv(filename):
     with open(filename, "r", newline="" , encoding='utf-8') as csvfile:
         reader = csv.reader(csvfile)
-        # 跳过第一行
-        next(reader)
+        data = []
+        # 读取整个文件
         for row in reader:
+            data.append(row)
+        # 更新同学成绩信息
+        for i in range(1, len(data)):
             regular_grade = randint(0, 100)
             final_grade = randint(0, 100)
             grade = calculate_grade(regular_grade, final_grade)
-            # 将同学信息写入文件
-            write_csv(filename, row[0], row[1], regular_grade, final_grade, grade)
+            # 在列表中更新同学成绩信息
+            data[i][2] = str(regular_grade)
+            data[i][3] = str(final_grade)
+            data[i][4] = str(grade)
+        # 将整个列表重新写入文件
+        write_csv(filename, data)
 
 # 写入csv文件
-def write_csv(filename, student_id, name, regular_grade, final_grade, grade):
-    with open(filename, "a", newline="" ,encoding='utf-8') as csvfile:
+def write_csv(filename, data):
+    with open(filename, "w", newline="" , encoding='utf-8') as csvfile:
         writer = csv.writer(csvfile)
-        # 注意需要将整数，浮点数转换为字符串再写入
-        writer.writerow([student_id, name, str(regular_grade), str(final_grade), str(grade)])
+        # 写入更新后的整个列表
+        for row in data:
+            writer.writerow(row)
 
 # 两个文件分别这样做一次
 read_csv("1-2班成绩表.csv")
